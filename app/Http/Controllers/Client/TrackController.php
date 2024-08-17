0c48a47cce63707bf9a937b1bb2774af41e96dc1
dmlcombd <modernad2008@gmail.com>
2024-08-17 17:27:06 +0600

    changes

--- app/Http/Controllers/Client/TrackController.php
+++ app/Http/Controllers/Client/TrackController.php
@@ -12,18 +12,44 @@ class TrackController extends Controller
     {
         if ($request->has('number')) {
             $trackNumber = $request->number;
-            $response = Http::get('http://localhost/dml-backend/public/api/track?number=' . $trackNumber);
+            $response = Http::get("http://127.0.0.1:8000/api/track?number=$trackNumber");
 
             if ($response->successful()) {
                 $bookingService = $response['booking']['service']['service'];
                 $awbNumber = $response['booking']['service']['awn'];
 
                 if ($bookingService === 'DPD') {
-                    $result = Http::get('https://apis.track.dpd.co.uk/v1/parcels/' . $awbNumber . '*20665/parcelevents');
+                    $result = Http::get("https://apis.track.dpd.co.uk/v1/parcels/$awbNumber*20665/parcelevents");
                 } else if ($bookingService === 'DHL') {
-                    $result = Http::get('https://api.dhl.com/track/shipments?trackingNumber=' . $awbNumber . '&find_by=tracking_number');
-                } else {
-                    $result = null;
+                    $result = Http::get("https://api.dhl.com/track/shipments?trackingNumber=$awbNumber&find_by=tracking_number");
+                } else if ($bookingService === 'FEDEX') {
+                    try {
+                        $getResponse = Http::asForm()->post('https://apis.fedex.com/oauth/token', [
+                            'grant_type' => 'client_credentials',
+                            'client_id' => 'l7e593a1ae415343689a5c4c4a5405bf06',
+                            'client_secret' => '88743b1256f642498ae9cf291a062d39'
+                        ]);
+
+                        $authToken = $getResponse->json();
+                        $authToken = $authToken['access_token'];
+
+                        $result = Http::withHeaders([
+                            'Authorization' => "Bearer $authToken",
+                            'X-locale' => 'en_US',
+                            'Content-Type' => 'application/json'
+                        ])->post('https://apis.fedex.com/track/v1/trackingnumbers', [
+                            'trackingInfo' => [
+                                [
+                                    'trackingNumberInfo' => [
+                                        'trackingNumber' => $awbNumber
+                                    ]
+                                ]
+                            ],
+                            'includeDetailedScans' => true
+                        ]);
+                    } catch (\Exception $ex) {
+                        echo $ex->getMessage();
+                    }
                 }
             } else {
                 $response = null;
@@ -38,8 +64,6 @@ class TrackController extends Controller
             $awbNumber = null;
             $result = null;
         }
-
         return view('client.track.track', compact('response', 'trackNumber', 'bookingService', 'awbNumber', 'result'));
     }
-
 }
--- resources/views/client/track/track.blade.php
+++ resources/views/client/track/track.blade.php
@@ -1,6 +1,6 @@
 @extends('client.layout.app')
 
-@section('title', 'Track Package - ' .  $trackNumber )
+@section('title', 'Track Package - ' . $trackNumber)
 
 @section('content')
     <section class="section d-block pt-5">
@@ -31,28 +31,42 @@
                     </form>
                 </div>
             </div>
-            <div style="max-width: 800px; margin: 0 auto; margin-bottom: 20px;" id="tracking-result">
+            <div style="max-width: 1000px; margin: 0 auto; margin-bottom: 20px;" id="tracking-result">
                 @if (!empty($response))
                     <div class="card shadow-lg border-0">
                         <div class="card-header bg-brand-1 text-white text-center" style="border-radius: 10px 10px 0 0;">
                             <h5 class="mb-0" style="font-size: 1.5rem; font-weight: 600; color: #034460">
-                                Tracking Result: {{ $trackNumber }}
+                                Tracking Details: {{ $trackNumber }}
                             </h5>
                         </div>
                         <div class="card-body p-4">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <p><strong>Shipper:</strong> {{ $response['booking']['sender']['name'] }}</p>
+                                    <p><strong>Country of Manufacture:</strong>
+                                        {{ $response['booking']['country_of_manufacture'] }}</p>
+                                    <p><strong>Post Code:</strong> {{ $response['booking']['sender']['post_code'] }}</p>
+                                </div>
+                                <div class="col-md-6">
+                                    <p><strong>Consignee:</strong>
+                                        {{ $response['booking']['receiver']['name'] }}
+                                    </p>
+                                    <p><strong>Destination:</strong>
+                                        {{ $response['booking']['receiver']['address'] }},
+                                        {{ $response['booking']['receiver']['city'] }},
+                                        {{ $response['booking']['receiver']['country_code'] }}
+                                    </p>
+                                    <p><strong>Zip Code:</strong>
+                                        {{ $response['booking']['receiver']['post_code'] }}
+                                    </p>
+                                </div>
+                            </div>
+                            <hr class="my-4">
                             <div class="row mb-3">
                                 <div class="col-md-6">
                                     <p><strong>Service:</strong> {{ $response['trakingUrl']['name'] }}</p>
-                                    <p class="status_txt">
-                                        Shipment Connected With {{ $response['trakingUrl']['name'] ?? 'N/A' }} with Ref
-                                        #<span class="ref_num">{{ $response['booking']['service']['awn'] }}</span>
-                                        <a target="_blank" style="color: #0073e6;"
-                                            href="{{ $response['trakingUrl']['url'] ?? '' }}">
-                                            view at {{ $response['booking']['service']['service'] ?? 'N/A' }} website
-                                            <i class="glyphicon glyphicon-new-window"></i>
-                                        </a>
-                                    </p>
-                                    <p><strong>AWN:</strong> {{ $response['booking']['service']['awn'] ?? 'N/A' }}</p>
+                                    <p><strong>AWB:</strong> {{ $response['booking']['service']['awn'] ?? 'N/A' }}</p>
+                                    <p><strong>Run:</strong> {{ $response['booking']['service']['run_no'] ?? 'N/A' }}</p>
                                 </div>
                                 <div class="col-md-6">
                                     <p><strong>Packages:</strong> {{ $response['booking']['packages'] }}</p>
@@ -61,24 +75,28 @@
                                 </div>
                             </div>
                             <hr class="my-4">
-                            <div class="row">
+                            <div class="row mb-3">
                                 <div class="col-md-6">
-                                    <p><strong>Good Values:</strong> ${{ $response['booking']['good_values'] }}</p>
-                                    <p><strong>Country of Manufacture:</strong>
-                                        {{ $response['booking']['country_of_manufacture'] }}</p>
-                                    <p><strong>Parcel Type:</strong> {{ $response['booking']['parcel_type'] }}</p>
+                                    <p><strong>Service Provider:</strong> {{ $response['trakingUrl']['name'] }} </p>
+                                    <p><strong>Address:</strong> </p>
                                 </div>
                                 <div class="col-md-6">
-                                    <p><strong>Created At:</strong>
-                                        {{ \Carbon\Carbon::parse($response['booking']['created_at'])->DiffForHumans() }}
-                                    </p>
-                                    <p><strong>Last Updated:</strong>
-                                        {{ \Carbon\Carbon::parse($response['booking']['updated_at'])->DiffForHumans() }}
-                                    </p>
+                                    <p><strong>Email:</strong></p>
+                                    <p><strong>Phone:</strong> </p>
                                 </div>
                             </div>
+
                         </div>
                     </div>
+                    {{-- {{ \Carbon\Carbon::parse($response['booking']['created_at'])->DiffForHumans() }} --}}
+                    <p class="status_txt text-center mt-3">
+                        Shipment Connected With {{ $response['trakingUrl']['name'] ?? 'N/A' }} with Ref
+                        #<span class="ref_num">{{ $response['booking']['service']['awn'] }}</span>
+                        <a target="_blank" style="color: #0073e6;" href="{{ $response['trakingUrl']['url'] ?? '' }}">
+                            view at {{ $response['booking']['service']['service'] ?? 'N/A' }} website
+                            <i class="glyphicon glyphicon-new-window"></i>
+                        </a>
+                    </p>
                 @else
                     <div class="card shadow-lg border-0">
                         <div class="card-body p-4 text-center">
@@ -89,31 +107,69 @@
             </div>
         </div>
     </section>
-
     <!-- New Section: Tracking Details -->
     <section style="max-width: 1000px; margin: 0 auto" class="section d-block">
         <div class="container">
             <div class="card shadow-lg border-0 mb-4">
                 <div class="card-header bg-brand-1 text-white text-center">
-                    <h5 class="mb-0" style="font-size: 1.5rem; font-weight: 600; color: #034460">Tracking Details</h5>
+                    <h5 class="mb-0" style="font-size: 1.5rem; font-weight: 600; color: #034460">Tracking Result</h5>
                 </div>
                 <div class="card-body p-4">
                     <div class="table-responsive">
                         <table class="table table-bordered">
                             <thead class="thead-light d-md-table-header-group">
                                 <tr>
-                                    <th>Status</th>
+                                    <th>Date</th>
+                                    <th>Time</th>
                                     <th>Last Location</th>
-                                    <th>Date and Time</th>
+                                    <th>Status</th>
                                 </tr>
                             </thead>
                             <tbody>
+                                @if ($bookingService === 'DPD' && isset($result['data']))
+                                    @foreach ($result['data'] as $event)
+                                        <tr class="d-block d-md-table-row">
+                                           
+                                            <td data-label="Date" class="d-block d-md-table-cell">
+                                                {{ \Carbon\Carbon::parse($event['eventDate'])->format('Y-m-d') }}</td>
+                                            <td data-label="Time" class="d-block d-md-table-cell">{{ $event['eventTime'] }}
+                                            </td>
+                                            <td data-label="Status" class="d-block d-md-table-cell">
+                                                {{ $event['eventText'] }}</td>
+                                            <td data-label="Location" class="d-block d-md-table-cell">
+                                                {{ $event['eventLocation'] }}</td>
+                                        </tr>
+                                    @endforeach
+                                @endif
+                                @if ($bookingService === 'FEDEX' && isset($result['output']['completeTrackResults']))
+                                    @foreach ($result['output']['completeTrackResults'][0]['trackResults'][0]['scanEvents'] as $event)
+                                        <tr class="d-block d-md-table-row">
+                                            <td data-label="Date" class="d-block d-md-table-cell">
+                                                {{ \Carbon\Carbon::parse($event['date'])->format('Y-m-d') }}</td>
+                                            <td data-label="Time" class="d-block d-md-table-cell">
+                                                {{ \Carbon\Carbon::parse($event['date'])->format('H:i:s') }}</td>
+                                            <td data-label="Location" class="d-block d-md-table-cell">
+                                                {{ $event['scanLocation']['city'] ?? 'N/A' }},
+                                                {{ $event['scanLocation']['stateOrProvinceCode'] ?? 'N/A' }}</td>
+                                            <td data-label="Status" class="d-block d-md-table-cell">
+                                                {{ $event['eventDescription'] }}</td>
+                                        </tr>
+                                    @endforeach
+                                @endif
                                 @if (isset($response['booking']['statuses']) && count($response['booking']['statuses']) > 0)
                                     @foreach ($response['booking']['statuses'] as $status)
                                         <tr class="d-block d-md-table-row">
-                                            <td data-label="Status" class="d-block d-md-table-cell">{{ $status['activities'] }}</td>
-                                            <td data-label="Last Location" class="d-block d-md-table-cell">{{ $status['location'] }}</td>
-                                            <td data-label="Date and Time" class="d-block d-md-table-cell">{{ \Carbon\Carbon::parse($status['date_time'])->format('Y-m-d H:i:s') }}</td>
+                                            <td data-label="Date" class="d-block d-md-table-cell">
+                                                {{ \Carbon\Carbon::parse($status['date_time'])->format('Y-m-d') }}
+                                            </td>
+                                            <td data-label="Time" class="d-block d-md-table-cell">
+                                                {{ \Carbon\Carbon::parse($status['date_time'])->format('H:i:s') }}
+                                            </td>
+                                            <td data-label="Last Location" class="d-block d-md-table-cell">
+                                                {{ $status['location'] }}</td>
+                                            <td data-label="Status" class="d-block d-md-table-cell">
+                                                {{ $status['activities'] }}</td>
+
                                         </tr>
                                     @endforeach
                                 @else
@@ -129,42 +185,7 @@
         </div>
     </section>
 
-    <!-- New Section: DPD Tracking Details -->
-    @if ($bookingService === 'DPD' && isset($result['data']))
-        <section style="max-width: 1000px; margin: 0 auto" class="section d-block">
-            <div class="container">
-                <div class="card shadow-lg border-0 mb-4">
-                    <div class="card-header bg-brand-1 text-white text-center">
-                        <h5 class="mb-0" style="font-size: 1.5rem; font-weight: 600; color: #034460">DPD Tracking Details</h5>
-                    </div>
-                    <div class="card-body p-4">
-                        <div class="table-responsive">
-                            <table class="table table-bordered">
-                                <thead class="thead-light d-md-table-header-group">
-                                    <tr>
-                                        <th>Status</th>
-                                        <th>Location</th>
-                                        <th>Date</th>
-                                        <th>Time</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($result['data'] as $event)
-                                        <tr class="d-block d-md-table-row">
-                                            <td data-label="Status" class="d-block d-md-table-cell">{{ $event['eventText'] }}</td>
-                                            <td data-label="Location" class="d-block d-md-table-cell">{{ $event['eventLocation'] }}</td>
-                                            <td data-label="Date" class="d-block d-md-table-cell">{{ \Carbon\Carbon::parse($event['eventDate'])->format('Y-m-d') }}</td>
-                                            <td data-label="Time" class="d-block d-md-table-cell">{{ $event['eventTime'] }}</td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-    @endif
+
 @endsection
 
 @push('styles')
